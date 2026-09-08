3a17db460eea381dada92c6a26fe917cf6f9b9d4
Sanket Nikat <program_sanketn@proteantech.in>
2026-09-08 14:55:08 +0530

    Your change description

---
+++ src/test/java/tests/sprint 1 and 2/App - Pre-Login - Login Screen/LoginTest.java
@@ -0,0 +1,31 @@
+package tests.sprint1;
+
+import framework.BaseTest;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test(description = "BD2M-672 - TC01 - Valid Login")
+    public void TC01_validLogin() {
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        step("Tap Login");
+        loginPage.clickLogin();
+
+        step("Enter valid mobile number");
+        loginPage.enterMobileNumber("7876765856");
+
+        step("Enter date of birth");
+        loginPage.enterDateOfBirth("07/09/2005");
+
+        step("Tap Login via OTP");
+        loginPage.clickLoginViaOtp();
+
+        step("Capture OTP screen");
+
+        System.out.println("========== OTP SCREEN ==========");
+        System.out.println(driver.getPageSource());
+        System.out.println("========== END OTP SCREEN ==========");
+    }
+}
